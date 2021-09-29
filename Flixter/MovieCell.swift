ae7dd15a0823a566cfba53f45355957d34610bd1
menukaghalan <59702166+menukaghalan@users.noreply.github.com>
2021-09-29 18:44:25 -0400

    First Commit
    First commit

---
+++ Flixter/MovieCell.swift
@@ -0,0 +1,32 @@
+//
+//  MovieCell.swift
+//  Flixter
+//
+//  Created by Menuka Ghalan on 9/29/21.
+//  Copyright © 2021 Menuka Ghalan. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
